eb7253d7f86da1bcdb3fe2b2fc54767258180b33
jessebrite <aboagye@protonmail.com>
2021-01-22 16:49:41 +0000

    added BookCategory Entoty

--- src/main/java/com/onus/onlinebookstore/entity/BookCategory.java
+++ src/main/java/com/onus/onlinebookstore/entity/BookCategory.java
@@ -1,4 +1,22 @@
 package com.onus.onlinebookstore.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Table(name = "tbl_category")
+@Entity
+@Data
 public class BookCategory {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(name = "category_name")
+	private String categoryName;
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+	private Set<Book> book;
 }
