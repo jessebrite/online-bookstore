289e194ee829ac324b642e3c1d647af843c96c5f
jessebrite <aboagye@protonmail.com>
2021-02-17 13:57:09 +0000

    cart-details component added

---
+++ bookstore-frontend/src/app/components/cart-details/cart-details.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+import { CartItem } from '@common/cart-item';
+import { CartService } from '@services/cart.service';
+
+@Component({
+  selector: 'app-cart-details',
+  templateUrl: './cart-details.component.html',
+  styleUrls: ['./cart-details.component.css'],
+})
+export class CartDetailsComponent implements OnInit {
+  cartItems: CartItem[] = [];
+  totalPrice = 0;
+  totalQuantity = 0;
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.cartDetails();
+  }
+
+  public cartDetails(): void {
+    this.cartItems = this.cartService.cartItems;
+
+    this.cartService.totalPrice.subscribe((data) => (this.totalPrice = data));
+
+    this.cartService.totalQuantity.subscribe(
+      (data) => (this.totalQuantity = data)
+    );
+
+    this.cartService.calculateTotalPrice();
+  }
+}
