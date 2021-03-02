f4e252d9b1a83dc40ab777ee9891a0a1498d21a9
jessebrite <aboagye@protonmail.com>
2021-03-02 20:55:01 +0000

    Security config setup

--- src/main/java/com/onus/onlinebookstore/config/SecurityConfig.java
+++ src/main/java/com/onus/onlinebookstore/config/SecurityConfig.java
@@ -1,4 +1,45 @@
 package com.onus.onlinebookstore.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.annotation.Resource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Resource(name = "userRepositoryUserDetailsService")
+	private UserDetailsService userDetailsService;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.formLogin()
+				.loginPage("/login")
+//				.usernameParameter("username")
+//				.permitAll();
+			.and()
+				.logout()
+				.logoutSuccessUrl("/");
+//				.permitAll();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService)
+			.passwordEncoder(passwordEncoder());
+	}
 }
