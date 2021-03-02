b230c6c9f8231fe8ac6a4fb79825ab3dde4bd2f5
jessebrite <aboagye@protonmail.com>
2021-03-02 20:54:07 +0000

    UserRepository added

--- src/main/java/com/onus/onlinebookstore/repository/UserRepository.java
+++ src/main/java/com/onus/onlinebookstore/repository/UserRepository.java
@@ -1,2 +1,8 @@
-package com.onus.onlinebookstore.repository;public class UserRepository {
+package com.onus.onlinebookstore.repository;
+
+import com.onus.onlinebookstore.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	User findByUsername(String username);
 }
