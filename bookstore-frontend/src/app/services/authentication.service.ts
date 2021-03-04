1ca30524843f6e31a1463cfac09cdd2e931e03b6
jessebrite <aboagye@protonmail.com>
2021-03-04 14:27:33 +0000

    authentication service implemented

--- bookstore-frontend/src/app/services/authentication.service.ts
+++ bookstore-frontend/src/app/services/authentication.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { ProcessErrorService } from '@services/process-error.service';
+import { User } from '@common/user';
+import { environment } from '@environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthenticationService {
+  url = `${environment.apiBaseUrl}/users`;
+  user = new User();
 
-  constructor() { }
+  headers = new HttpHeaders()
+    .set('Content-type', 'application/json')
+    .set('Accept', 'application/json');
+
+  constructor(
+    private httpClient: HttpClient,
+    private processErrorService: ProcessErrorService
+  ) {}
+
+  public processOrder(user: User): Observable<User> {
+    console.log('user: ', user);
+    return this.httpClient
+      .post<User>(this.url, user, {
+        headers: this.headers,
+      })
+      .pipe(
+        tap((results: User) => console.log('results: ', results)),
+        catchError(this.processErrorService.processError)
+      );
+  }
 }
