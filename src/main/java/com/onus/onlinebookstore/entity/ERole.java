b6c19ae8cba5b32653207b56bbec6056e8c3bdbb
jessebrite <aboagye@protonmail.com>
2021-03-06 22:56:46 +0000

    added AuthToken Filter

--- src/main/java/com/onus/onlinebookstore/entity/ERole.java
+++ src/main/java/com/onus/onlinebookstore/entity/ERole.java
@@ -1,4 +1,7 @@
 package com.onus.onlinebookstore.entity;
 
-public class ERole {
+public enum ERole {
+	ROLE_USER,
+	ROLE_MODERATOR,
+	ROLE_ADMIN
 }
--- src/main/java/com/onus/onlinebookstore/entity/Role.java
+++ src/main/java/com/onus/onlinebookstore/entity/Role.java
@@ -1,2 +1,18 @@
-package com.onus.onlinebookstore.entity;public class Role {
+package com.onus.onlinebookstore.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(length = 20)
+	private ERole name;
+
 }
