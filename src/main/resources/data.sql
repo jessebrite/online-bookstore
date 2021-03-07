95d65f0a648b409d813588badbbf7c3af541361c
jessebrite <aboagye@protonmail.com>
2021-03-07 23:28:51 +0000

    Changes to schema.sql and data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT IGNORE INTO `book-store-dev`.`authorities`
+		VALUES (NULL, 'anonymous', 'write'),
+					(NULL, 'user', 'write'),
+					(NULL, 'jondoe', 'read'),
+					(NULL, 'jantrumaen', 'write'),
+					(NULL, 'john', 'read');
+
+INSERT IGNORE INTO `book-store-dev`.`users`
+    VALUES (NULL, 'John', 'Snow', 'anonymous', 'anonyous@eg.com', 'password', true),
+           (NULL, 'Steve', 'Gyan', 'user', 'user@user.com', 'secret', true),
+           (NULL, 'Steve', 'Asante', 'jondoe', 'jondoe@gold.ca', '12345', true),
+           (NULL, 'Baah', 'Akyeamfour', 'jantrumaen', 'jantru@maen.me', '5ecret', false),
+           (NULL, 'Anim', 'Asante', 'anim1', 'anim1@mine.ca', 'word', true);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,25 @@
+-- Below are the user and other related tables.
+-- Yet to fine-tune it to my needs and put into production
+-- what we have are skeletons populated with dummy data
+
+-- use only once before production
+-- DROP TABLE IF EXISTS `book-store-dev`.`users`;
+--
+-- CREATE TABLE IF NOT EXISTS `book-store-dev`.`users` (
+--     `id` INT NOT NULL AUTO_INCREMENT,
+--     `firstname` VARCHAR(25) NOT NULL,
+--     `lastname` VARCHAR(45) NOT NULL,
+--     `username` VARCHAR(25) NOT NULL,
+--     `email` VARCHAR(45) NOT NULL,
+--     `password` VARCHAR(120) NOT NULL,
+--
+--     `enabled` BOOLEAN NOT NULL,
+--     PRIMARY KEY (`id`));
+--
+-- DROP TABLE IF EXISTS `book-store-dev`.`authorities`;
+--
+-- CREATE TABLE IF NOT EXISTS `book-store-dev`.`authorities` (
+-- 	 `id` INT NOT NULL AUTO_INCREMENT,
+-- 	 `username` VARCHAR(45) NULL,
+-- 	 `authority` VARCHAR(45) NULL,
+-- 	 PRIMARY KEY (`id`));
