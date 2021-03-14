a1295e5d5021d37462396d5d9d792c800c304c17
jessebrite <aboagye@protonmail.com>
2021-03-14 23:43:04 +0000

    Validation constraints added

--- bookstore-frontend/src/app/common/order.ts
+++ bookstore-frontend/src/app/common/order.ts
@@ -1,14 +1,17 @@
 import { CartItem } from './cart-item';
+import { User } from './user';
 
 export class Order {
-  public firstname = '';
-  public lastname = '';
-  public email = '';
-  public address = '';
+	public firstname = '';
+	public lastname = '';
+	public email = '';
+	public address = '';
   public city = '';
   public state = '';
   public zip = '';
   public country = '';
   public shipped = false;
   cartItem: CartItem[] = [];
+
+  // constructor(public user: User) {}
 }
