3fa66eb662f6cbead43268246ce2f91552f43d12
jessebrite <aboagye@protonmail.com>
2021-03-18 11:22:52 +0000

    AuthGuard implemented for checkout

---
+++ bookstore-frontend/src/app/guards/auth.guard.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+const USER_KEY = 'auth-user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (window.sessionStorage.getItem(USER_KEY)) {
+      return true;
+    }
+
+    this.router.navigate(['login'], { queryParams: { returnUrl: state.url } });
+    return false;
+  }
+}
