abb789db2576306f719acc1f74b0f806fea8627a
jessebrite <aboagye@protonmail.com>
2021-03-19 22:23:46 +0000

    admin guard implemented

---
+++ bookstore-frontend/src/app/guards/admin.guard.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { TokenStorageService } from '@services/token-storage.service';
+import { environment } from '@environments/environment';
+
+const USER_KEY = environment.user_key;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AdminGuard implements CanActivate {
+  constructor(private router: Router, private tokenService: TokenStorageService) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot,
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    const user = this.tokenService.getCurrentUser();
+    if (user) {
+      if (route.data?.roles.indexOf(user.roles) !== -1) {
+        return true;
+      }
+    }
+
+    this.router.navigate(['login'], { queryParams: { returnUrl: state.url } });
+    return false;
+  }
+}
