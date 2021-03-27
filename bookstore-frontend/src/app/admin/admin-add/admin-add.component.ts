ff0acb027ec5b697f4156be0ca09a256d2ed7411
jessebrite <aboagye@protonmail.com>
2021-03-27 22:44:44 +0000

    minor tweaks

--- bookstore-frontend/src/app/admin/admin-add/admin-add.component.ts
+++ bookstore-frontend/src/app/admin/admin-add/admin-add.component.ts
@@ -12,17 +12,17 @@ import { Book } from '@common/book';
 })
 export class AdminAddComponent implements OnInit {
   book: Book = new Book();
-	bookSent = false;
-	submitted = false;
+  bookSent = false;
+  submitted = false;
 
   constructor(private bookService: AdminBookService, private router: Router) {}
 
   ngOnInit(): void {}
 
   public onSubmit(form: NgForm): void {
-					this.submitted = true;
+    this.submitted = true;
     if (form.valid) {
-						this.bookSent = true;
+      this.bookSent = true;
       this.addBook(this.book);
     }
   }
--- bookstore-frontend/src/app/admin/admin-routing.module.ts
+++ bookstore-frontend/src/app/admin/admin-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AdminComponent } from './admin.component';
+// import { AdminComponent } from './admin.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { AdminAddComponent } from './admin-add/admin-add.component';
 import { AdminEditComponent } from './admin-edit/admin-edit.component';
@@ -10,7 +10,7 @@ const routes: Routes = [
   { path: 'dashboard', component: AdminDashboardComponent },
   { path: 'addBook', component: AdminAddComponent },
   { path: 'editBook/:id', component: AdminEditComponent },
-  { path: '', component: AdminComponent },
+  { path: '', redirectTo: 'dashboard' },
 ];
 
 @NgModule({
